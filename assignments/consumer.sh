334ea317f7a0f05de65dc2c164451a176cf47d9e
Kartik Gopalan <gopalan.kartik@gmail.com>
2017-03-24 18:05:55 -0400

    foo

---
+++ assignments/consumer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+	echo "Usage: " $0 "<linepipe name>"
+	exit 1
+fi
+
+if [[ ! -p $1 ]]; then
+    # Students: when testing your character device implementation, replace the following line with your module insertion code 
+    if   ! mkfifo $1; then
+	exit 1
+    fi
+fi
+
+while true
+do
+    if read line <$1; then
+        echo $line
+    fi
+done
+
---
+++ assignments/producer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+	echo "Usage: " $0 "<linepipe name>"
+	exit 1
+fi
+
+# Students: When testing your character device, change this to test for the presence of your character device
+if [[ ! -p $1 ]]; then
+    echo $1 "not present OR not a pipe"
+    exit 1
+fi
+
+for i in {1..100}
+do
+    line="Producer $$ Line $i" 
+    echo "Writing " $line "to " $1
+    echo $line >$1
+    sleep 1
+done
+
+
