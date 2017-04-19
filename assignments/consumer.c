39cf5df031ff2328005bc2daf13f206c4d9ef458
Kartik Gopalan <gopalan.kartik@gmail.com>
2017-04-19 17:47:17 -0400

    foo

--- assignments/consumer.c
+++ assignments/consumer.c
@@ -18,12 +18,12 @@ int main(int argc, char *argv[])
 	if( argc != 2) {
 		printf("Usage: %s <linepipe_name>\n", argv[0]);
 		exit(1);
-	}	
+	}
 
 	if ( (fp = fopen(argv[1], "r")) == NULL) {
 		perror(""); printf("error opening %s\n", argv[1]);
 		exit(1);
-	} 
+	}
 
 	while(1) {
 		// read a line
@@ -35,7 +35,6 @@ int main(int argc, char *argv[])
 			printf("line: %s", line);
 			printf("bytes read: %ld\n", ret);
 		}
-
 	}
 	fclose(fp);
 
