01b3f1f4784d0630da8a138b7237dab854f9885c
Kevin Cheng <tcheng8@binghamton.edu>
2017-09-05 20:37:38 -0400

    Include an example of function pointer
    new file:   function_pointer_example.c

---
+++ examples/function_pointer_example.c
@@ -0,0 +1,26 @@
+/**
+ * The C program demonstrates how to use a function pointer.
+ * @author Kevin Cheng
+ * @since  09/05/2017
+ */
+
+#include <stdio.h>
+
+/* "func" is a pointer to a function that expects an integer argument and
+ * returns an integer.
+ */
+int (*func)(int *);
+
+/* Mimic the pre-increment operator. What about the post increment operator? */
+int pre_increment(int *x) {
+  (*x) += 1;
+  return *x;
+}
+
+/* Let's have some fun with the function pointer. */
+int main(void) {
+  func = &pre_increment; // From the C99 specification, func = pre_increment
+  int x = 5;
+  printf("%d\n", (*func)(&x));
+  return 0;
+}
