bd60c9bd20cee4a5fce62e992ae731b03c39a4b2
Kartik Gopalan <kartik@binghamton.edu>
2017-11-08 17:42:21 -0500

    minor changes

--- assignments/consumer_numbers.c
+++ assignments/consumer_numbers.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 		// read a line
 		ssize_t ret = read(fd, &num_read, sizeof(int));
 		if( ret > 0) {
-			printf("Number read: %d", num_read);
+			printf("Number read: %d ", num_read);
 			printf("Bytes read: %ld\n", ret);
 		} else {
 			fprintf(stderr, "error reading ret=%ld errno=%d perror: ", ret, errno);
