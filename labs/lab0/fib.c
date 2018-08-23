ab4ccf8dcf1600c4dd23a5e0b19b5552b91c6b01
Kartik Gopalan <gopalan.kartik@gmail.com>
2018-08-22 23:21:16 -0400

    foo

--- labs/lab0/fib.c
+++
@@ -1,8 +0,0 @@
-#include "fib.h"
-
-int fib(int number)
-{
-    if (number <= 2)
-        return 1;
-    return fib(number-1) + fib(number-2);
-}
--- labs/lab0/fib.h
+++
@@ -1,6 +0,0 @@
-#ifndef FIB_H
-#define FIB_H
-
-int fib(int number);
-
-#endif //FIB_H
