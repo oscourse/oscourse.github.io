5dd5dc3aed99c06d7febb04bf13350dd9bc18eda
Kartik Gopalan <gopalan.kartik@gmail.com>
2019-08-26 12:28:39 -0400

    foo

--- examples/fork_ex.c
+++ examples/fork_ex.c
@@ -7,22 +7,32 @@
 int main()
 {
 	pid_t pid;
+	int status;
+	int ret;
 
 	pid = fork();
  
-	if (pid == -1) { 
-		fprintf(stderr, "fork failed\n"); 
+	if (pid < 0) { 
+		perror("fork failed:"); 
 		exit(1); 
 	} 
 
-	if (pid == 0) { 
+	if (pid == 0) { // Child executes this block
 		printf("This is the child\n"); 
 		exit(0); 
 	} 
 
-	if (pid > 0) { 
+	if (pid > 0) { //Parent executes this block
 		printf("This is parent. The child is %d\n", pid); 
-		exit(0); 
+
+		ret = waitpid(pid, &status, 0);
+		if (ret < 0) {
+			perror("waitpid failed:");
+			exit(2); 
+		}
+
+
+		printf("Child exited with status %d\n", status);
 	} 
 
 	return 0;
