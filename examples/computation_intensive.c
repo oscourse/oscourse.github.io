b5adf94d30f38b28e24dad0a61257b683e7ac8eb
Spoorti Doddamani <sdoddam1@cs.binghamton.edu>
2019-10-24 19:37:48 -0400

    adding test applications

---
+++ examples/computation_intensive.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <time.h>
+#include <limits.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define MAX_NUM 1000000
+
+int ifPrime(int num){
+	int i;
+	if(num <= 1)
+		return 0;
+
+	for(i = 2; i < num; i++){
+		if((num % i) == 0)
+			return 0;
+	}
+	
+	return 1;
+}
+
+void *get_prime_count(){
+	int num, primeCount = 0;
+	//for(num = 0; num < INT_MAX; num++){
+	for(num = 0; num < MAX_NUM; num++){
+		if(ifPrime(num))
+			primeCount++;
+	}
+
+	//return primeCount;
+}
+
+int main(){
+
+	int primeCount, num_threads = 5, i;
+
+	time_t start, end;
+
+	pthread_t tid[num_threads];
+	
+	start = time(NULL);
+
+	for(i = 0; i < num_threads; i++){
+		pthread_create(&tid[i], NULL, get_prime_count, NULL);
+	}
+
+	for(i = 0; i < num_threads; i++){
+		pthread_join(tid[i], NULL);
+	}
+ 
+	//primeCount = get_prime_count();
+	
+	end = time(NULL);
+
+	printf("Time taken to find prime numbers: %ld seconds\n", end - start);
+
+	while(1);
+
+	return 0;
+}
+
---
+++ examples/memory_intensive.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	unsigned long **matrix;
+	int rows = 100, columns = 512;
+	int i, j;
+
+	printf("size of unsigned long %zu\n", sizeof(unsigned long));
+	
+	matrix = (unsigned long**)malloc(rows * sizeof(unsigned long*));
+	if(!matrix)
+		perror("malloc() failed\n");
+
+	for(i = 0; i < rows; i++){
+		matrix[i] = (unsigned long*)malloc(sizeof(unsigned long) * columns);
+		if(!matrix[i])
+			perror("malloc() failed\n");
+	}
+
+	// First for loop
+	for(i = 0; i < rows; i++){
+		for(j = 0; j < columns; j++){
+			matrix[i][j] = 0;
+		}
+	}
+
+	// Second for loop
+	for(j = 0; j < columns; j++){
+		for(i = 0; i < rows; i++){
+			matrix[i][j] = 5;
+		}
+	}
+
+	// What is the difference between first and second for loop?
+	// How many pages are allocated for the matrix?
+	// Which loop performs better?
+	// What happens if the number of rows increase?
+	// What happens if the number of columns increase?
+	
+	while(1);
+
+	for(i = 0; i < rows; i++)
+		free(matrix[i]);
+
+	free(matrix);
+
+	return 0;
+}
